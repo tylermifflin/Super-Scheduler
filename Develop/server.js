f4739b93785a316848800544a50519d8d5ce9c70
Tyler Mifflin <tylermifflin@Tylers-MacBook-Air.local>
2023-05-03 20:38:52 -0600

    using app.use to call express.urlencoded and express.json

--- Develop/server.js
+++ Develop/server.js
@@ -1,9 +1,15 @@
+// calling express, path and fs
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
-const uuid = require('./helpers/uuid');
+// calling uuid to generate unique id for each note
 const { v4: uuidv4 } = require('uuid');
-
+// using process.env.port to allow heroku to set port
 const PORT = process.env.port || 3001;
-
+// calling express in app
 const app = express();
+// using express to parse incoming string or array data
+app.use(express.urlencoded({ extended: true }));
+// using express to parse incoming JSON data
+app.use(express.json());
+
