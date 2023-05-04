10794e2957fda7c90498c07e59c122f1d2c80e4d
Tyler Mifflin <tylermifflin@Tylers-MacBook-Air.local>
2023-05-03 20:29:15 -0600

    called the uuid in my server.js

--- Develop/server.js
+++ Develop/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const fs = require('fs');
 const uuid = require('./helpers/uuid');
+const { v4: uuidv4 } = require('uuid');
 
 const PORT = process.env.port || 3001;
 
